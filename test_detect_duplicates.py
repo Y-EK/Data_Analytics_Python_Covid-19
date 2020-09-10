fc96a10f49ff62a33ed939f4001d26c0e63a7386
Youness <elkhatabi.youness@gmail.com>
2020-09-10 23:16:30 +0200

    add explanation of tests introduced

--- test_detect_duplicates.py
+++ test_detect_duplicates.py
@@ -1,4 +1,5 @@
-# 
+# Nous rappelons ici la commande pour installer 
+# le package python pytest :
 # pip install -U pytest
 # pip install pandas -U
 
@@ -8,8 +9,14 @@ from pandas._testing import assert_frame_equal
 from detect_duplicates_script import detect_duplicates
 import datetime
 
+# chemin absolut du fichier csv utilisé comme référentiel des codes postaux
 path_referential = './data_post_codes.csv';
 
+
+# Dans ce test nous vérifions que la fonction detect_duplicates 
+# supprime les duplications en respectons la condition :
+# garder la ligne avec le moins de valeurs nulles (non valides) au niveau des 
+# champs postcode et date_of_birth
 def test_delete_duplicates():
     
     df = pd.DataFrame([
@@ -32,7 +39,26 @@ def test_delete_duplicates():
     assert_frame_equal(left=df_expected.reset_index(drop=True), right=df_result.reset_index(drop=True),
                        check_dtype=False)
 
+# Ici on vérifie que notre fonction supprime les lignes avec un patient_id contenant une valeur nulle.    
+def test_delete_duplicates_nv():
+    
+    df = pd.DataFrame([
+        [104136, 812, 'NT', 19801222, 40],
+        [np.nan, 4221, 'QLD', 19771201, 43],
+        [None, 2087, 'NSW', 19330815, 87]
+        ], columns = ['patient_id', 'postcode', 'state', 'date_of_birth', 'age'])
+    
+    df_result = detect_duplicates(df, path_referential)
+    
+    df_expected = pd.DataFrame([
+        [104136, 812, 'NT', datetime.datetime.strptime('1980-12-22', '%Y-%m-%d'), 40]
+        ], columns = ['patient_id', 'postcode', 'state', 'date_of_birth', 'age'])
+    
+    assert_frame_equal(left=df_expected.reset_index(drop=True), right=df_result.reset_index(drop=True),
+                       check_dtype=False)
 
+# Dans ce test on vérifie que notre fonction calcule correctement l'age
+# à partir de la date de naissance.
 def test_get_corrected_age():
         
     df = pd.DataFrame([
@@ -51,7 +77,10 @@ def test_get_corrected_age():
     
     assert_frame_equal(left=df_expected.reset_index(drop=True), right=df_result.reset_index(drop=True),
                        check_dtype=False)  
-    
+
+# Complément du test précédent : 
+# ici nous vérifions que notre fonction ne calcul l'age que
+# lorsque la date de naissance correspond à une date de naissance valide
 def test_get_corrected_age_wdob():
         
     df = pd.DataFrame([
@@ -69,6 +98,8 @@ def test_get_corrected_age_wdob():
     assert_frame_equal(left=df_expected.reset_index(drop=True), right=df_result.reset_index(drop=True),
                        check_dtype=False)   
     
+# Dans le test ci-dessous, on vérifie que notre fonction
+# associe le nom de la région lorsqu'un code postale correct est fournit
 def test_get_corrected_state(): 
     
     df = pd.DataFrame([
@@ -88,6 +119,8 @@ def test_get_corrected_state():
     assert_frame_equal(left=df_expected.reset_index(drop=True), right=df_result.reset_index(drop=True),
                        check_dtype=False)    
 
+# Ici on s'assure que la fonction associe la valeur "UNKNOWN" à toutes valeurs
+# érronée du code postal.
 def test_get_corrected_state_wpc(): 
     
     df = pd.DataFrame([
