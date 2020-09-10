dac2a5973fb07b50f2782de05e1940fb1b946a64
Youness <elkhatabi.youness@gmail.com>
2020-09-10 21:28:20 +0200

    delete non valid data according to date_of_birth patient_id and postcode

--- get_final_dataset.py
+++ get_final_dataset.py
@@ -13,10 +13,10 @@ def get_cleaned_df_pcr(data):
     # Conversion en majuscule 
     df_pcr_cleaned.loc[:,'pcr'] = df_pcr_cleaned['pcr'].str.upper()
     # Normalisation de la colonne pcr
-    # 'negative' <- 0 
+    # 'negative' <=== 0 
     df_pcr_cleaned["pcr"].replace({"N": 0}, inplace=True)
     df_pcr_cleaned["pcr"].replace({"NEGATIVE": 0}, inplace=True)
-    # 'postive' <- 1
+    # 'postive' <=== 1
     df_pcr_cleaned["pcr"].replace({"P": 1}, inplace=True)
     df_pcr_cleaned["pcr"].replace({"POSITIVE": 1}, inplace=True)
     #
@@ -31,14 +31,20 @@ def get_final_ds(data_patient, data_pcr, path_referential):
     # data frame obtenu après un processus de data cleaning appliqué au jeu de donnée
     # sur les données relatifs aux contamination par le Covid19
     df_cleaned_pcr = get_cleaned_df_pcr(data_pcr)
-    
+    # On fusionne les dataframes de patients (dédupliquée) avec celle des résultats des test du Covid19
     df_final_ds = pd.merge(df_cleaned_pcr,
                            df_patient_dedup,
                            on='patient_id', 
                            how='left')
     
-    # Introduction d'une clonne pour les categories d'âge
-    #df_final_ds
+    # Pour fiabiliser notre analyse statistique, nous éliminons toutes les lignes
+    # avec des valeurs manquantes ou érronées sur l'une des colonnes : 
+    # date_of_birth, postcode et patient_id.
+    # (Rappelons que dans cette exemple toutes les valeurs de patient_id sont valides)
+    df_final_ds = df_final_ds[df_final_ds['patient_id'].notnull() 
+                              & df_final_ds['date_of_birth'].notnull() 
+                              & df_final_ds['postcode'].notnull() 
+                              & df_final_ds['postcode'] != 0]
     
     return df_final_ds
     
